6389dbb8966f2c84598ecc4c5f8bfb805489017f
Hydroponey <hydroponey@localhost>
2020-04-28 12:49:32 +0200

    ft_strrchr

---
+++ srcs/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/28 11:53:35 by asimoes           #+#    #+#             */
+/*   Updated: 2020/04/28 12:49:07 by asimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*last_occur;
+
+	last_occur = NULL;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			last_occur = s;
+		s++;
+	}
+	return (last_occur);
+}
