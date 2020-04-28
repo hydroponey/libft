52724bc1cfe905093f95eb6c9ca00161f8696012
Hydroponey <hydroponey@localhost>
2020-04-28 12:52:32 +0200

    cast char* on s

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 11:53:35 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/28 12:49:07 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/28 12:52:05 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strrchr(const char *s, int c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			last_occur = s;
+			last_occur = (char*)s;
 		s++;
 	}
 	return (last_occur);
