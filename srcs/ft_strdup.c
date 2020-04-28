3ca955941674aa2db51e750ee900d18d7e154899
Hydroponey <hydroponey@localhost>
2020-04-28 22:28:29 +0200

    ft_strdup fixing the starting index to get length of str

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 21:53:33 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/28 22:01:33 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/28 22:27:28 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strdup(const char *s)
 	size_t	len;
 	char	*dup;
 
-	len = 1;
+	len = 0;
 	while (s[len])
 		len++;
-	if (!(dup = malloc(len * sizeof(char))))
+	if (!(dup = malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	len = 0;
 	while (s[len])
