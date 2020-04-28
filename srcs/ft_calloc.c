83b2959bf669e09dc2ffd5ad6ec47aa0a682733c
Hydroponey <hydroponey@localhost>
2020-04-28 23:46:26 +0200

    ft_calloc modified to return 0 if nmemb or size equals 0

--- srcs/ft_calloc.c
+++ srcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 22:18:15 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/28 22:31:34 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/28 23:45:59 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,15 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	char	*data;
 	size_t	i;
 
-	if (nmemb == 0)
+	if (nmemb == 0 || size == 0)
 		return (0);
-	if (size == 0)
-	{
-		if (!(data = malloc(1)))
-			return (0);
-		return (data);
-	}
 	if (!(data = malloc(nmemb * size)))
 		return (0);
 	i = 0;
 	while (i < size)
+	{
 		data[i] = 0;
+		i++;
+	}
 	return (data);
 }
