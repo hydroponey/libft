5d102d0e8d05b39b2d54c9adf5dec77811d6eac6
Hydroponey <hydroponey@localhost>
2020-04-28 12:58:29 +0200

    fix ft_strrchr when c = \0

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 11:53:35 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/28 12:52:05 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/28 12:57:19 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ char	*ft_strrchr(const char *s, int c)
 			last_occur = (char*)s;
 		s++;
 	}
+	if (*s == c)
+		return ((char*)s);
 	return (last_occur);
 }
