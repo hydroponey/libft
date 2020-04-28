23f3b210ae616e944d1210100224f2c6cf8cf94e
Hydroponey <hydroponey@localhost>
2020-04-28 13:15:50 +0200

    ft_strncmp fixed

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 11:48:13 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/28 13:09:48 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/28 13:15:22 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,13 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-    unsigned int i;
+	size_t	i;
 
-    i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0' && i < n)
-    {
-        if (s1[i] != s2[i])
-            return (unsigned char)(s1[i] - s2[i]);
-        i++;
-    }
-    if ((s2[i] != '\0' || s1[i] != '\0') && i < n)
-        return (unsigned char)(s1[i] - s2[i]);
-    return (0);
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+		i++;
+	if (n > 0)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	else
+		return (0);
 }
