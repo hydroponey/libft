ce7050a4f7d58f158cc3d9dccba2d2136a47288a
Hydroponey <hydroponey@localhost>
2020-04-29 15:42:30 +0200

    ft_memmove fixed

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 18:00:54 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/29 15:25:54 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/29 15:41:54 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,22 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*s;
 	unsigned char	*d;
 
+	if (dest == src)
+		return (dest);
 	s = (unsigned char*)src;
 	d = (unsigned char*)dest;
 	i = 0;
 	if (src < dest)
-	{
+		while (i < n)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
+	else
 		while (i < n)
 		{
 			d[i] = s[i];
 			i++;
 		}
-	}
 	return (dest);
 }
