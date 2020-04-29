460d04feb874e68e029fdfb6c8231e3989b8ad3a
Hydroponey <hydroponey@localhost>
2020-04-29 15:31:08 +0200

    ft_memmove modified and ft_putnbr_fd try

--- srcs/ft_putnbr_fd.c
+++ srcs/ft_putnbr_fd.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/04/27 18:00:54 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/29 15:25:54 by asimoes          ###   ########.fr       */
+/*   Created: 2020/04/29 15:28:13 by asimoes           #+#    #+#             */
+/*   Updated: 2020/04/29 15:29:26 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int	i;
-	unsigned char	*s;
-	unsigned char	*d;
-
-	s = (unsigned char*)src;
-	d = (unsigned char*)dest;
-	i = 0;
-	if (src < dest)
-	{
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
-	}
-	return (dest);
+	write(fd, &n, sizeof(int));
 }
