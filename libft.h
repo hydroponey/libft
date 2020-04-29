7deb13f8df0d1f3f4e2c39cd7db345b31868620e
Hydroponey <hydroponey@localhost>
2020-04-29 03:10:04 +0200

    ft_putchar_fd, ft_putendl_fd, ft_putstr_fd and libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 14:52:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/28 12:12:55 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/04/29 00:42:30 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 void	*ft_memset(char *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
@@ -39,5 +40,16 @@ char	ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
