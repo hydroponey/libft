6ad4ac6217a39ac1bad66331391233e3b555bc37
Hydroponey <hydroponey@localhost>
2020-05-05 18:01:20 +0200

    ft_lstadd_back ok

---
+++ srcs/ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/05 17:44:29 by asimoes           #+#    #+#             */
+/*   Updated: 2020/05/05 18:00:50 by asimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	unsigned int	i;
+
+	if (alst[0] == NULL)
+	{
+		alst[0] = new;
+		return ;
+	}
+	i = 0;
+	while (alst[i]->next != NULL)
+		i++;
+	alst[i]->next = new;
+}
