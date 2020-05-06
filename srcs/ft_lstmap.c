5096029f5b6e7f503889ef2dd34f7bc7523e3241
Hydroponey <hydroponey@localhost>
2020-05-06 23:07:40 +0200

    lstmap test

--- srcs/ft_lstmap.c
+++ srcs/ft_lstmap.c
@@ -6,8 +6,38 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 22:20:33 by asimoes           #+#    #+#             */
-/*   Updated: 2020/05/05 17:21:29 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/06 23:07:13 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*cur;
+	t_list	*new;
+	t_list	*start;
+
+	if (!lst)
+		return (NULL);
+	new = NULL;
+	start = NULL;
+	while (lst)
+	{
+		if (!(cur = malloc(sizeof(t_list))))
+			return (NULL);
+		cur->content = f(lst->content);
+		cur->next = NULL;
+		if (new != NULL)
+			new->next = cur;
+		else
+		{
+			start = cur;
+			new = cur;
+		}
+		if (del != NULL)
+			del(lst->content);
+		lst = lst->next;
+	}
+	return (start);
+}
