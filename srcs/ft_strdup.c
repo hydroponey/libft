6c44adfd7128bfab04e388f577859c2dfa38c6a3
Hydroponey <hydroponey@localhost>
2020-05-06 17:02:33 +0200

    using ft_strlen instead of a while to get the length of s

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/28 21:53:33 by asimoes           #+#    #+#             */
-/*   Updated: 2020/04/29 04:06:04 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/06 15:28:56 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ char	*ft_strdup(const char *s)
 	size_t	len;
 	char	*dup;
 
-	len = 0;
-	while (s[len])
-		len++;
+	len = ft_strlen(s);
 	if (!(dup = malloc((len + 1) * sizeof(char))))
 		return (NULL);
 	len = 0;
