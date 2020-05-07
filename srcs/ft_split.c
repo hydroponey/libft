8938f29b6f9ac62b1ad885750a7ad34db0c9c701
Hydroponey <hydroponey@localhost>
2020-05-07 20:07:17 +0200

    ft_split fixing the length for the while and using get_eleme_count to allocate memory

--- srcs/ft_split.c
+++ srcs/ft_split.c
@@ -6,83 +6,95 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 22:13:35 by asimoes           #+#    #+#             */
-/*   Updated: 2020/05/04 22:14:01 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/07 20:06:22 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strndup(char *str, unsigned int n)
+char    *ft_strndup(char *str, unsigned int n)
 {
-	unsigned int	len;
-	unsigned int	max;
-	char			*copy;
-	unsigned int	i;
+    unsigned int    len;
+    unsigned int    max;
+    char            *copy;
+    unsigned int    i;
 
-	len = ft_strlen(str);
-	max = (len <= n) ? len : n;
-	if (!(copy = malloc(sizeof(char) * (max + 1))))
-		return (NULL);
-	i = 0;
-	while (i < max)
-	{
-		copy[i] = str[i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
+    len = ft_strlen(str);
+    max = (len <= n) ? len : n;
+    if (!(copy = malloc(sizeof(char) * (max + 1))))
+        return (NULL);
+    i = 0;
+    while (i < max)
+    {
+        copy[i] = str[i];
+        i++;
+    }
+    copy[i] = '\0';
+    return (copy);
 }
 
-int		get_string_size(char *str, char c)
+int     get_string_size(const char *str, char c)
 {
-	int i;
+    int i;
 
-	i = 0;
-	while (str[i] != '\0' && str[i] != c)
-		i++;
-	return (i);
+    i = 0;
+    while (str[i] != '\0' && str[i] != c)
+        i++;
+    return (i);
 }
 
-int		get_elem_count(char *str, char c)
+int		get_elem_count(const char *str, char c)
 {
 	int	i;
 	int	count;
+	int	len;
+	int	string_len;
 
+	len = ft_strlen(str);
 	i = 0;
+	string_len = 0;
 	count = 0;
-	while (str[i])
+	while (str[i] && i < len)
 	{
-		count++;
-		i += get_string_size(&str[i], c);
+		string_len = get_string_size(&str[i], c);
+		if (string_len == 0)
+		{
+			i += 1;
+		}
+		else
+		{
+			i += string_len;
+			count++;
+		}
 	}
 	return (count);
 }
 
-char	**ft_split(const char *str, char c)
+char    **ft_split(const char *str, char c)
 {
-	char	**tab;
-	int		i;
-	int		j;
-	int		len;
-	int		string_len;
+    char    **tab;
+    int     i;
+    int     j;
+    int     len;
+    int     string_len;
 
 	if (!str || str[0] == '\0')
 		return (NULL);
-	if ((tab = malloc(sizeof(char *) * (ft_strlen(str) + 1))) == NULL)
-		return (NULL);
-	i = 0;
-	j = 0;
-	len = ft_strlen(str) + 1;
-	while (i < len)
-	{
-		if (str[i] != c)
-		{
-			string_len = get_string_size((char *)&str[i], c);
-			tab[j++] = ft_strndup((char *)&str[i], string_len);
-			i += string_len;
-		}
+    if ((tab = malloc(sizeof(char *) * (get_elem_count(str, c) + 1))) == NULL)
+        return (NULL);
+    i = 0;
+    j = 0;
+    len = ft_strlen(str);
+    while (i < len)
+    {
+        if (str[i] != c)
+        {
+            string_len = get_string_size(&str[i], c);
+            tab[j++] = ft_strndup((char *)&str[i], string_len);
+            i += string_len;
+        }
 		i++;
-	}
-	tab[j] = 0;
-	return (tab);
+    }
+    tab[j] = NULL;
+    return (tab);
 }
