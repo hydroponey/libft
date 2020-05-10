7a4acd8b91de4d9499783d1341f9a223c6beedee
Hydroponey <hydroponey@localhost>
2020-05-10 05:12:31 +0200

    Rearranging list functions order to fit with the subject

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 14:52:21 by asimoes           #+#    #+#             */
-/*   Updated: 2020/05/07 21:33:58 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/07 21:36:32 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,9 +60,9 @@ void			ft_putnbr_fd(int n, int fd);
 
 t_list			*ft_lstnew(void *content);
 void			ft_lstadd_front(t_list **alst, t_list *new);
-void			ft_lstadd_back(t_list **alst, t_list *new);
 int				ft_lstsize(t_list *lst);
 t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void (*f)(void *));
