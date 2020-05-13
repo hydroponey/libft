cf86aaa889f8a307981102a314d12c873000dada
Hydroponey <hydroponey@localhost>
2020-05-13 19:01:03 +0200

    ft_strlcat norme and var name fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 13:11:17 by asimoes           #+#    #+#             */
-/*   Updated: 2020/05/13 18:59:55 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/13 19:00:51 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,15 @@ size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
 	srclen = ft_strlen(src);
 	dstlen = ft_strlen(dst);
 	dstlen = (dstlen < dstsize) ? dstlen : dstsize;
-	if (dstlen == dstsize) return dstsize+srclen;
+	if (dstlen == dstsize)
+		return (dstsize + srclen);
 	i = 0;
 	if (srclen < dstsize-dstlen)
 		ft_memcpy(dst+dstlen, src, srclen+1);
 	else
 	{
-		ft_memcpy(dst+dstlen, src, maxlen-1);
+		ft_memcpy(dst+dstlen, src, dstsize-1);
 		dst[dstlen + dstsize - 1] = '\0';
 	}
-	return dstlen + srclen;	
+	return (dstlen + srclen);	
 }
