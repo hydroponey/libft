b08a566297f894de9534c235201edbe9be37670b
Hydroponey <hydroponey@localhost>
2020-05-13 19:01:40 +0200

    ft_strlcat norme

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/02 13:11:17 by asimoes           #+#    #+#             */
-/*   Updated: 2020/05/13 19:00:51 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/05/13 19:01:34 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,11 @@ size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
 	if (dstlen == dstsize)
 		return (dstsize + srclen);
 	i = 0;
-	if (srclen < dstsize-dstlen)
-		ft_memcpy(dst+dstlen, src, srclen+1);
+	if (srclen < dstsize - dstlen)
+		ft_memcpy(dst + dstlen, src, srclen + 1);
 	else
 	{
-		ft_memcpy(dst+dstlen, src, dstsize-1);
+		ft_memcpy(dst + dstlen, src, dstsize - 1);
 		dst[dstlen + dstsize - 1] = '\0';
 	}
 	return (dstlen + srclen);	
