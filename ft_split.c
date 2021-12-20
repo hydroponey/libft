4a4871856de37b133ed885ed9d629917e81ca925
asimoes <asimoes@student.42.fr>
2021-12-20 13:46:31 +0100

    Fix norme for split and substr

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,11 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 04:06:50 by asimoes           #+#    #+#             */
-/*   Updated: 2021/12/19 16:35:24 by asimoes          ###   ########.fr       */
+/*   Updated: 2021/12/20 13:45:32 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static char	*ft_strndup(const char *str, unsigned int n)
-{
-	unsigned int	len;
-	unsigned int	max;
-	char			*copy;
-	unsigned int	i;
-
-	len = ft_strlen(str);
-	if (len < n)
-		max = len;
-	else
-		max = n;
-	copy = (char *)malloc(sizeof(char) * (max + 1));
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (i < max)
-	{
-		copy[i] = str[i];
-		i++;
-	}
-	copy[i] = '\0';
-	return (copy);
-}
 
 static void	ft_freetab(char **tab)
 {
@@ -69,45 +44,44 @@ static int	get_elem_count(const char *s, char c)
 	return (count);
 }
 
-static char	*copy_str(const char *s, const char *end)
+static short int	split(char **tab, char const *s, char c, unsigned int *i)
 {
-	size_t			len;
-
-	if (!end)
-		len = ft_strlen(s);
-	else
-		len = (size_t)(end - s);
-	return (ft_strndup(s, len));
-}
-
-char	**ft_split(char const *s, char c)
-{
-	char			**tab;
 	const char		*end;
-	unsigned int	i;
 
-	tab = malloc(sizeof(char *) * (get_elem_count(s, c) + 1));
-	if (!s || !tab)
-		return (NULL);
-	i = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
 			end = ft_strchr(s, c);
-			tab[i] = copy_str(s, end);
-			if (!tab[i])
+			if (!end)
+				tab[*i] = ft_substr(s, 0, ft_strlen(s));
+			else
+				tab[*i] = ft_substr(s, 0, (end - s));
+			if (!tab[*i])
 			{
 				ft_freetab(tab);
-				return (NULL);
+				return (0);
 			}
-			i++;
+			(*i)++;
 			if (!end || c == '\0')
 				break ;
 			s = end;
 		}
 		s++;
 	}
+	return (1);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char			**tab;
+	unsigned int	i;
+
+	tab = malloc(sizeof(char *) * (get_elem_count(s, c) + 1));
+	if (!s || !tab)
+		return (NULL);
+	i = 0;
+	split(tab, s, c, &i);
 	tab[i] = NULL;
 	return (tab);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/29 03:11:27 by asimoes           #+#    #+#             */
-/*   Updated: 2021/12/19 10:05:02 by asimoes          ###   ########.fr       */
+/*   Updated: 2021/12/19 22:05:51 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (!sub)
-		return (NULL);
 	if (ft_strlen(s) <= start)
 	{
+		sub = (char *)malloc(sizeof(char) * 1);
+		if (!sub)
+			return (NULL);
 		sub[0] = '\0';
 		return (sub);
+	}
+	else
+	{
+		sub = (char *)malloc(sizeof(char) * (len + 1));
+		if (!sub)
+			return (NULL);
 	}
 	ft_strlcpy(sub, s + start, len + 1);
 	return (sub);
